5eee58275ad106be8c0ca503dbfa3733f72aebd9
Nguyễn Việt <25522062@gm.uit.edu.vn>
2026-04-03 20:14:34 +0700

    Update Snake.cpp

--- Snake.cpp
+++ Snake.cpp
@@ -33,6 +33,21 @@ public:
 
     }
 };
+#define MINX 2
+#define MINY 2
+#define MAXX 35
+#define MAXY 20
+
+void VeKhung() {
+    for (int i = MINX; i <= MAXX; i++) {
+        for (int j = MINX; j <= MAXY; j++) {
+            if ((i == MINX) || (i == MAXX) || (j == MINY) || (j == MAXY)) {
+                gotoxy(i, j);
+                printf("+");
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -49,6 +64,7 @@ int main()
             if (t=='x') Huong = 1;
         }
         system("cls");
+        VeKhung();
         r.Ve();
         r.DiChuyen(Huong);
         Sleep(300);
